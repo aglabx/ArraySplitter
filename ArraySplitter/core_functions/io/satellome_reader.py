fa29e32dfb955e3dcef361e8adfa29b364ecb78f
Aleksey Komissarov <ad3002@gmail.com>
2024-02-21 01:48:13 +0200

    initial commit

---
+++ ArraySplitter/core_functions/io/satellome_reader.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @created: 20.02.2024
+# @author: Aleksey Komissarov
+# @contact: ad3002@gmail.com
+
+from ArraySplitter.core_functions.models.trf_model import TRModel
+from ArraySplitter.core_functions.io.tab_file import sc_iter_tab_file
+
+
+def sc_iter_arrays_satellome_file(trf_file):
+    """Iter over satellome file."""
+    for trf_obj in sc_iter_tab_file(trf_file, TRModel):
+        yield trf_obj.trf_array
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import re
+
+version = None
+for line in open("./ArraySplitter/__init__.py"):
+    m = re.search("__version__\s*=\s*(.*)", line)
+    if m:
+        version = m.group(1).strip()[1:-1]  # quotes
+        break
+assert version
+
+setup(
+    name='ArraySplitter',
+    version=version,
+    packages=["ArraySplitter"],
+    package_data={"": ["README.md"]},
+    python_requires='>=3.6',
+    include_package_data=True,
+    scripts=[],
+    license="BSD",
+    url='https://github.com/aglabx/ArraySplitter',
+    author='Aleksey Komissarov',
+    author_email='ad3002@gmail.com',
+    description='De Novo Decomposition of Satellite DNA Arrays into Monomers within Telomere-to-Telomere Assemblies',
+    install_requires=[
+        'PyExp',
+        'intervaltree',
+        'editdistance',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+)
